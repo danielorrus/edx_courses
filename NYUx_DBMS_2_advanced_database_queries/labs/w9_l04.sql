bb394f2c6b2e3751dc4873a7fa19be97dd0271f9
danielorrus <drgponte@gmail.com>
2021-12-05 11:44:38 +0100

    completado curso NYUx NYUx.DBMS.2 Advanced Database Queries

---
+++ NYUx_DBMS_2_advanced_database_queries/labs/w9_l04.sql
@@ -0,0 +1,15 @@
+/* Write a SELECT statement that returns these columns from the Orders table:
+    - The order_id column
+    - The order_date column
+    - A column named approx_ship_date that’s calculated by adding 2 days to the order_date column
+    - The ship_date column if it doesn’t contain a null value
+    - A column named days_to_ship that shows the number of days between the order date and the ship date
+
+add a WHERE clause that retrieves just the orders for March 2018 that have shipped.
+
+Sort the result set by the order_id column in ascending sequence. */
+
+SELECT order_id, order_date, DATE_ADD(order_date, INTERVAL 2 DAY) AS approx_ship_date, IF(ISNULL(ship_date),"", ship_date) AS ship_date,
+	IF(ISNULL(ship_date),"", DATEDIFF(ship_date, order_date)) AS days_to_ship
+FROM orders
+-- ORDER BY discount_amount ASC
