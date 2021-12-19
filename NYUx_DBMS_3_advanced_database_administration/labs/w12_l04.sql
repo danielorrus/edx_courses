0b58c3f97d410c2666b3a91ff0ed4d7ed68a7f9a
danielorrus <drgponte@gmail.com>
2021-12-19 21:50:34 +0100

    terminada w12

--- NYUx_DBMS_3_advanced_database_administration/labs/w12_l04.sql
+++ NYUx_DBMS_3_advanced_database_administration/labs/w12_l04.sql
@@ -1,10 +1,8 @@
-/* Create a view named product_summary that uses the same table you used in in exercise 2. This view should return
-summary information about each product.
-Each row should include product_name, order_count (the quantity of the product that has been ordered)
-and order_total (the total sales revenue for the product). */
+/* Create a view named best_products that uses the same table you used in in exercise 3.
+The view should have the same columns returned. The view should only return the five best selling products by order_total. */
 
 USE my_guitar_shop;
-/* VISTA CREADA EN LAB ANTERIOR:
+/* VISTA CREADA EN LAB 2:
 CREATE OR REPLACE VIEW order_item_products AS
 	SELECT
 		o.order_id,
@@ -21,6 +19,7 @@ CREATE OR REPLACE VIEW order_item_products AS
 		INNER JOIN products AS p
 			ON oi.product_id  = p.product_id */
 
+/* VISTA CREADA EN LAB 3:
 CREATE OR REPLACE VIEW product_summary AS
 	SELECT
 		product_name,
@@ -28,3 +27,9 @@ CREATE OR REPLACE VIEW product_summary AS
         SUM(item_total) AS order_total
 	FROM order_item_products
     GROUP BY product_name
+*/
+
+-- CREATE OR REPLACE VIEW best_products AS
+	SELECT *
+    FROM product_summary
+    ORDER BY order_total DESC LIMIT 5
